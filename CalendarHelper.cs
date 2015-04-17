e73d4f348fcdc46515a7d41ee92e4d84d9e00847
wiil <wiilx86x@gmail.com>
2015-04-17 09:06:21 +0200

    test

---
+++ CalendarHelper.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+using SilverlightCalendar.Model;
+
+namespace SilverlightCalendar
+{
+    public class CalendarHelper
+    {
+        public static SolidColorBrush NormalCalendarItemBrush = new SolidColorBrush(Color.FromArgb(255, 51, 51, 51));
+        public static SolidColorBrush TeamMemberCalendarItemBrush = new SolidColorBrush(Color.FromArgb(255, 0, 204, 122));
+        public static SolidColorBrush SelectedCalendarItemBrush = new SolidColorBrush(Color.FromArgb(255, 255, 128, 64));
+        public static SolidColorBrush AbsentCalendarItemBrush = new SolidColorBrush(Color.FromArgb(255, 158, 43, 62));
+        public static SolidColorBrush CalendarItemSumBrush = new SolidColorBrush(Color.FromArgb(255, 111, 153, 53));
+        public static SolidColorBrush InvalidCalendarItemBrush = new SolidColorBrush(Color.FromArgb(255, 7, 94, 148));
+        public static SolidColorBrush CalendarDataGridHolidayBrush = new SolidColorBrush(Color.FromArgb(136, 136, 255, 0));
+        public static SolidColorBrush CalendarDataGridWeekendBrush = new SolidColorBrush(Color.FromArgb(40, 51, 180, 255));
+        public static SolidColorBrush CalendarDataGridMainBackground = new SolidColorBrush(Color.FromArgb(255, 255, 255, 255));
+        
+        public static SolidColorBrush DefaultItemBrush = new SolidColorBrush(Color.FromArgb(255, 70, 130, 180));
+        public static SolidColorBrush KrankItemBrush = new SolidColorBrush(Color.FromArgb(255, 255, 0, 255));
+
+        public static Dictionary<int, SolidColorBrush> AuftragsartenColors { get; set; }
+        public static BaseDataObject BaseData = new BaseDataObject();
+        public static FilterObject CurrentCalendarItemFilter { get; set; }
+        public static Dictionary<int, string> AuftragsArtenBezeichung { get; set; }   
+
+        public static SolidColorBrush IsValidOpacity(SolidColorBrush brush, bool valid)
+        {
+            if (brush != null)
+            {
+                SolidColorBrush solidColorBrush = new SolidColorBrush();
+                solidColorBrush.Color = brush.Color;
+                if (valid)
+                { solidColorBrush.Opacity = 1.0; }
+                else
+                { solidColorBrush.Opacity = 0.3; }
+                return solidColorBrush;
+            }
+
+            return null;
+        }
+
+        public static SolidColorBrush GetCalendarItemBackground(object parameter)
+        {
+            string status = parameter.ToString();
+            if (status == "DefaultCalendarItem")
+            {
+                return NormalCalendarItemBrush;
+            }
+            else if (status == "TeamMember")
+            {
+                return TeamMemberCalendarItemBrush;
+            }
+            else if (status == "DefaultItem")
+            {
+                return DefaultItemBrush;
+            }
+            else if (status == "Krank")
+            {
+                return KrankItemBrush;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static SolidColorBrush GetItemBackground(int auftragsArtId)
+        {
+            if (CalendarHelper.AuftragsartenColors.ContainsKey(auftragsArtId))
+            {
+                return AuftragsartenColors[auftragsArtId];
+            }
+            else
+            {
+                return DefaultItemBrush;
+            }
+        }
+
+        public static Uri GetImageUri(string relativeImagePath)
+        {
+            CalendarHelper moduleHelper = new CalendarHelper();
+            Uri imageUri = new Uri("/" + moduleHelper.GetType().Assembly.ToString().Split(',')[0] + ";component/" + relativeImagePath, UriKind.Relative);
+
+            return imageUri;
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Telerik.Windows.Controls.DragDrop;
+using SilverlightCalendar.SilverlightCalendarService;
+using System.Collections.ObjectModel;
+using SilverlightCalendar.Model;
+using Reisewitz.Core.Framework;
+
+namespace SilverlightCalendar
+{
+    public partial class MainPage : UserControl
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Framework.Current = new CalendarFramework();
+
+            Calendar.ViewModel = new CalendarControlViewModel(); ;
+            Calendar.ItemContextMenuOpening += new ItemContextMenuOpeningEventHandler(Calendar_ItemContextMenuOpening);
+            Calendar.CalendarItemContextMenuOpening += new CalendarItemContextMenuOpeningEventHandler(Calendar_CalendarItemContextMenuOpening);
+            //RadDragAndDropManager.AddDragQueryHandler(TestListBox, OnDragQueryTest);
+            this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+            
+            SilverlightCalendarService.SilverlightCalendarServiceClient client = new SilverlightCalendarService.SilverlightCalendarServiceClient();
+            ServiceCalendarItemFilter filter = new ServiceCalendarItemFilter();
+            filter.DateMin = DateTime.Today.AddDays(-6);
+            filter.DateMax = DateTime.Today.AddDays(14);
+            filter.LoadItems = true;
+            Calendar.ViewModel.LoadByFilter(filter);
+            Calendar.ViewModel.LoadBaseData();
+        }
+
+        void Calendar_CalendarItemContextMenuOpening(object sender, CalendarItem item, ContextMenu contextMenu)
+        {
+            contextMenu.Items.Add(new MenuItem() { Header = "abc" });
+        }
+
+        void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            //if (Calendar.ViewModel.Rows != null)
+            //{
+            //    foreach (var x in Calendar.ViewModel.Rows.FirstOrDefault().Items.FirstOrDefault().Items)
+            //    {
+            //        TestListBox.Items.Add(x);
+            //    }
+            //}
+        }
+
+        void Calendar_ItemContextMenuOpening(object sender, Item item, ContextMenu contextMenu)
+        {
+            contextMenu.Items.Add(new MenuItem() { Header = "xyz" });
+            contextMenu.Items.Add(new MenuItem() { Header = "123" });
+        }       
+
+        private void OnDragQueryTest(object sender, DragDropQueryEventArgs e)
+        {
+            var item = (e.Source as ListBox).SelectedItem;
+            if (e.Options.Status == DragStatus.DragQuery && item != null)
+            {
+                e.Options.ArrowCue = RadDragAndDropManager.GenerateArrowCue();
+                e.Options.Payload = item;
+                ContentControl cc = new ContentControl();
+                cc.Content = item;
+                e.Options.DragCue = cc;
+            }
+
+            e.QueryResult = true;
+            e.Handled = true;
+        }
+
+        private void OnDragInfoTest(object sender, DragDropEventArgs e)
+        {
+
+        }
+    }
+}
